2349bb3c129a0433150047fc1b91074b79d319bc
zhujiyu <zhuhz82@126.com>
2014-08-04 17:31:59 +0800

    做好了安装�
    大面上应该没问题了。

--- CeilingDesigner/AupuReportForm.Designer.cs
+++ CeilingDesigner/AupuReportForm.Designer.cs
@@ -31,6 +31,7 @@
             this.components = new System.ComponentModel.Container();
             Microsoft.Reporting.WinForms.ReportDataSource reportDataSource1 = new Microsoft.Reporting.WinForms.ReportDataSource();
             Microsoft.Reporting.WinForms.ReportDataSource reportDataSource2 = new Microsoft.Reporting.WinForms.ReportDataSource();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AupuReportForm));
             this.reportGoodsBindingSource = new System.Windows.Forms.BindingSource(this.components);
             this.reportDataSet = new CeilingDesigner.ReportDataSet();
             this.photosBindingSource = new System.Windows.Forms.BindingSource(this.components);
@@ -76,8 +77,10 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(784, 562);
             this.Controls.Add(this.reportViewer1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "AupuReportForm";
             this.Text = "报表";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.Load += new System.EventHandler(this.AupuReportForm_Load);
             ((System.ComponentModel.ISupportInitialize)(this.reportGoodsBindingSource)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.reportDataSet)).EndInit();
